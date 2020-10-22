61cf6ca87655168581db5c659ae22dfc5be5299c
Administrator <835683840@qq.com>
2020-10-22 13:47:47 +0800

    763. 划分字母区间

--- app/src/main/java/com/example/algorithmdemo/Explain.java
+++ app/src/main/java/com/example/algorithmdemo/Explain.java
@@ -6,7 +6,7 @@ public class Explain {
     20201020(重排链表):给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
              将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
              你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
-    20201021:
-
+    20201021：925. 长按键入。利用双指针
+    20201022：划分字母区间 (https://leetcode-cn.com/problems/partition-labels/)
      */
 }
