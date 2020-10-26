e52b8a39fe6c373fbf40a09b9cbfe3206b5dbefa
Administrator <835683840@qq.com>
2020-10-26 11:03:13 +0800

    1365. 有多少小于当前数字的数字

---
+++ app/src/test/java/com/example/algorithmdemo/LeetCode20201026Test.java
@@ -0,0 +1,40 @@
+package com.example.algorithmdemo;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * LeetCode20201022Test
+ *
+ * https://leetcode-cn.com/problems/reorder-list/
+ */
+public class LeetCode20201026Test {
+    @Test
+    public void test() {
+        int[] array = new int[]{6,6,6,6};
+        smallerNumbersThanCurrent(array);
+    }
+
+
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[][] array = new int[nums.length][1];
+        int[] array1 = new int[nums.length];
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i]<nums[j]){
+                    array[j][0]++;
+                }else if (nums[i]>nums[j]){
+                    array[i][0]++;
+                }
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            array1[i] = array[i][0];
+            System.out.println(array1[i]);
+        }
+        return array1;
+    }
+
+}
