0bfa29622eeb7c01f8c9406ac229dd503f030478
xumingming <835683840@qq.com>
2020-11-14 14:04:56 +0800

    mac同步

--- rpc1/src/main/java/com/xmm/rpc1/MainActivity.java
+++ rpc1/src/main/java/com/xmm/rpc1/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
