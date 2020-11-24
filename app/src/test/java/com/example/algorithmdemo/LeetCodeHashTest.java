0a7e1ea9d3566a2525a04c410dc1f82ee8065655
Administrator <835683840@qq.com>
2020-11-24 10:31:01 +0800

    完全二叉树节点数和hash算法

--- app/src/test/java/com/example/algorithmdemo/LeetCodeHashTest.java
+++ app/src/test/java/com/example/algorithmdemo/LeetCodeHashTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,7 +17,27 @@ public class LeetCodeHashTest {
 
     @Test
     public void test() {
+        int[] ints = {2, 2, 1,3,3,4,5,5,4};
+        System.out.println(singleNumber(ints));
 
+
+    }
+
+    /**
+     * 136. 只出现一次的数字 (官方解答 用位运算更快)
+     */
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            Integer integer = map.get(num);
+            if (integer == null){
+                map.put(num,1);
+            }else {
+                map.remove(num);
+            }
+        }
+        return map.keySet().iterator().next();
 
     }
 
