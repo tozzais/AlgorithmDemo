e907ab4f9c2c51bb9c2f0fd17ecfeef8cf5a4395
Administrator <835683840@qq.com>
2020-12-03 10:53:14 +0800

    204. 计数质数 和 228. 汇总区间

--- app/src/test/java/com/example/algorithmdemo/LeetCodeArrayTest.java
+++ app/src/test/java/com/example/algorithmdemo/LeetCodeArrayTest.java
@@ -3,8 +3,10 @@ package com.example.algorithmdemo;
 import org.junit.Test;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -151,8 +153,29 @@ public class LeetCodeArrayTest {
 
     @Test
     public void test2() {
-        int[] a = new int[]{1,2,3,1,2,3,1};
-        System.out.println(containsNearbyDuplicate(a,2));
+        int[] a = new int[]{1,2,3,67};
+        System.out.println(summaryRanges(a));
+    }
+
+    /**
+     * 228. 汇总区间
+     * @param nums
+     * @return
+     */
+    public List<String> summaryRanges(int[] nums) {
+        List<String> summary = new ArrayList<>();
+        for (int i, j = 0; j < nums.length; ++j){
+            i = j;
+            // try to extend the range [nums[i], nums[j]]
+            while (j + 1 < nums.length && nums[j + 1] == nums[j] + 1)
+                ++j;
+            // put the range into the list
+            if (i == j)
+                summary.add(nums[i] + "");
+            else
+                summary.add(nums[i] + "->" + nums[j]);
+        }
+        return summary;
     }
 
 }
