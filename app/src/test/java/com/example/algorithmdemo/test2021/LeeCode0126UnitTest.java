ce85d02156d3b9e8e18f0924ef0d6a487ddbcbf0
tozzais <835683840@qq.com>
2021-01-26 15:11:37 +0800

    959. 由斜杠划分区域

--- app/src/test/java/com/example/algorithmdemo/test2021/LeeCode0126UnitTest.java
+++ app/src/test/java/com/example/algorithmdemo/test2021/LeeCode0126UnitTest.java
@@ -2,75 +2,32 @@ package com.example.algorithmdemo.test2021;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
- 959. 由斜杠划分区域
+ 1128. 等价多米诺骨牌对的数量
  */
 public class LeeCode0126UnitTest {
     @Test
     public void test() {
-        int[] a = new int[]{2,1,5};
+        int[][] a = new int[][]{{1,2},{1,2},{1,2},{1,4}};
+        System.out.println(numEquivDominoPairs(a));
     }
 
-    public int regionsBySlashes(String[] grid) {
-        int n = grid.length;
-        int[] f = new int[n * n * 4];
-        for (int i = 0; i < n * n * 4; i++) {
-            f[i] = i;
-        }
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] num = new int[100];
+        int ret = 0;
+        for (int[] domino : dominoes) {
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int idx = i * n + j;
-                if (i < n - 1) {
-                    int bottom = idx + n;
-                    merge(f, idx * 4 + 2, bottom * 4);
-                }
-                if (j < n - 1) {
-                    int right = idx + 1;
-                    merge(f, idx * 4 + 1, right * 4 + 3);
-                }
-                if (grid[i].charAt(j) == '/') {
-                    merge(f, idx * 4, idx * 4 + 3);
-                    merge(f, idx * 4 + 1, idx * 4 + 2);
-                } else if (grid[i].charAt(j) == '\\') {
-                    merge(f, idx * 4, idx * 4 + 1);
-                    merge(f, idx * 4 + 2, idx * 4 + 3);
-                } else {
-                    merge(f, idx * 4, idx * 4 + 1);
-                    merge(f, idx * 4 + 1, idx * 4 + 2);
-                    merge(f, idx * 4 + 2, idx * 4 + 3);
-                }
-            }
+            int val = domino[0] < domino[1] ? domino[0] * 10 + domino[1] : domino[1] * 10 + domino[0];
+            ret += num[val];
+            num[val]++;
         }
+        return ret;
 
-        Set<Integer> fathers = new HashSet<Integer>();
-        for (int i = 0; i < n * n * 4; i++) {
-            int fa = find(f, i);
-            fathers.add(fa);
-        }
-        return fathers.size();
-    }
-
-    public int find(int[] f, int x) {
-        if (f[x] == x) {
-            return x;
-        }
-        int fa = find(f, f[x]);
-        f[x] = fa;
-        return fa;
-    }
-
-    public void merge(int[] f, int x, int y) {
-        int fx = find(f, x);
-        int fy = find(f, y);
-        f[fx] = fy;
     }
 
 }
