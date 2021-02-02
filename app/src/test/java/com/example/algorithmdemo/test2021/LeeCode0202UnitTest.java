197c4845dd0212f560551bbe945893178a3ace10
tozzais <835683840@qq.com>
2021-02-02 09:50:02 +0800

    424. 替换后的最长重复字符

---
+++ app/src/test/java/com/example/algorithmdemo/test2021/LeeCode0202UnitTest.java
@@ -0,0 +1,38 @@
+package com.example.algorithmdemo.test2021;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+/**
+ 424. 替换后的最长重复字符
+ */
+public class LeeCode0202UnitTest {
+    @Test
+    public void test() {
+        characterReplacement("AAAAABCD",2);
+    }
+
+    public int characterReplacement(String s, int k) {
+        int[] num = new int[26];
+        int n = s.length();
+        int maxn = 0;
+        int left = 0, right = 0;
+        while (right < n) {
+            num[s.charAt(right) - 'A']++;
+            maxn = Math.max(maxn, num[s.charAt(right) - 'A']);
+            if (right - left + 1 - maxn > k) {
+                num[s.charAt(left) - 'A']--;
+                left++;
+            }
+            right++;
+        }
+        return right - left;
+
+    }
+}
